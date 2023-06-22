7ea33bb49568a784810def31ae903a0aa8f07756
hk2257853 <hk2257853@gmail.com>
2023-06-22 22:11:20 +0530

    minor changes to Deleteconformationmodel for ux

--- Driver Details Service/client/src/Components/Userlist/DeleteConfirmationModal.jsx
+++ Driver Details Service/client/src/Components/Userlist/DeleteConfirmationModal.jsx
@@ -2,8 +2,8 @@ function DeleteConfirmationModal({ selectedRowName, closeModal, handleConfirmDel
     return (
       <div className="fixed inset-0 flex items-center justify-center z-10">
         <div className="bg-white p-6 rounded-md shadow-lg">
-          <p>Are you sure you want to delete {selectedRowName}?</p>
-          <div className="mt-4 flex justify-end">
+          <p>Are you sure you want to delete <span className="font-bold">{selectedRowName}'s </span> data?</p>
+          <div className="mt-4 flex justify-center">
             <button className="mr-2 px-4 py-2 bg-gray-200 text-gray-700 rounded-md" onClick={closeModal}>
               Cancel
             </button>
