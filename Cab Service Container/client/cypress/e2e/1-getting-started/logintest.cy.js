9816197fa3996a62c114f046f6bf3454fa30eb7d
hk2257853 <hk2257853@gmail.com>
2023-07-12 23:52:28 +0530

    cypress e2e testing added for FE

--- Cab Service Container/client/cypress/e2e/1-getting-started/logintest.cy.js
+++ Cab Service Container/client/cypress/e2e/1-getting-started/logintest.cy.js
@@ -12,25 +12,9 @@ describe('test_name', function() {
   
      cy.get('.min-h-screen > .items-center > .mt-8 > .w-full > #email').click()
   
-     cy.get('.min-h-screen > .items-center > .mt-8 > .w-full > #email').type('2@2.com')
+     cy.get('.min-h-screen > .items-center > .mt-8 > .w-full > #email').type('1@1.com')
   
-     cy.get('.min-h-screen > .items-center > .mt-8 > .w-full > #password').type('2')
-  
-   //   cy.get('.min-h-screen > .items-center > .mt-8 > .w-full > .appearance-none').click()
-  
-     cy.get('.min-h-screen > .items-center > .mt-8 > .w-full > .appearance-none').select('driver')
-  
-   //   cy.get('.min-h-screen > .items-center > .mt-8 > .w-full > .appearance-none').click()
-  
-     cy.get('#\__next > .min-h-screen > .items-center > .mt-8 > .w-full:nth-child(4)').click()
-  
-     cy.get('.divide-y:nth-child(1) > .py-2 > .flex > .w-6:nth-child(2) > svg').click()
-  
-     cy.get('.mt-8 > .w-full > .flex > .w-full > #driver-name').click()
-  
-     cy.get('.mt-8 > .w-full > .flex > .w-full > #driver-name').type('asdf')
-  
-     cy.get('.fixed > .flex > .inline-block > .bg-gray-50 > .w-full').click()
+     cy.get('.min-h-screen > .items-center > .mt-8 > .w-full > #password').type('12345678')     
   
   })
  
